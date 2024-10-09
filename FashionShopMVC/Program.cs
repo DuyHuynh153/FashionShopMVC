1cc44045c6ba896b2902b257ed89b2c8aab7f851
PhanMyDung1 <mydungdaisy2210@gmail.com>
2024-10-09 19:24:28 +0700

    Hien thi san pham cua cua hang

--- FashionShopMVC/Program.cs
+++ FashionShopMVC/Program.cs
@@ -1,27 +1,143 @@
+﻿using FashionShopMVC.Repositories;
+using FashionShopMVC.Data;
+using FashionShopMVC.Models.Domain;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.FileProviders;
+using AspNetCoreHero.ToastNotification;
+using AspNetCoreHero.ToastNotification.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddControllersWithViews();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization();
+
+
+// Register Database
+builder.Services.AddDbContext<FashionShopDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Register Controller
+//builder.Services.AddScoped<IUserRepository, UserRepository>();
+//builder.Services.AddScoped<ITokenRepository, TokenRepository>();
+//builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+//builder.Services.AddScoped<IContactRepository, ContactRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+//builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+//builder.Services.AddScoped<IVoucherRepository, VoucherRepository>();
+//builder.Services.AddScoped<IFavoriteProductRepository, FavoriteProductRepository>();
+//builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+//builder.Services.AddScoped<IProvinceRepository, ProvinceRepository>();
+//builder.Services.AddScoped<IDistrictRepository, DistrictRepository>();
+//builder.Services.AddScoped<IWardRepository, WardRepository>();
+//builder.Services.AddScoped<IStatisticRepository, StatisticRepository>();
+
+builder.Services.AddNotyf(
+    config => {
+        config.DurationInSeconds = 10;
+        config.IsDismissable = true;
+        config.Position = NotyfPosition.TopRight;
+    });
+
+
+//Register service authentication
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(option =>
+//{
+//    option.TokenValidationParameters = new TokenValidationParameters
+//    {
+//        ValidateIssuer = true,
+//        ValidateAudience = true,
+//        ValidateLifetime = true,
+//        ValidateIssuerSigningKey = true,
+//        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+//        ValidAudience = builder.Configuration["Jwt:Audience"],
+//        ClockSkew = TimeSpan.Zero,
+//        IssuerSigningKey = new SymmetricSecurityKey(
+//            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]
+//            ))
+//    };
+//});
+
+// Config identity user
+//builder.Services.AddIdentityCore<User>()
+//    .AddRoles<IdentityRole>()
+//    .AddTokenProvider<DataProtectorTokenProvider<User>>("FashionShop")
+//    .AddEntityFrameworkStores<FashionShopDBContext>()
+//    .AddDefaultTokenProviders();
+
+
+// Config password register
+//builder.Services.Configure<IdentityOptions>(option =>
+//{
+//    // Thiết lập về Password
+//    option.Password.RequireDigit = false; // Không bắt phải có số
+//    option.Password.RequireLowercase = false; // Không bắt phải có chữ thường
+//    option.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
+//    option.Password.RequireUppercase = false; // Không bắt buộc chữ in
+//    option.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
+//    option.Password.RequiredUniqueChars = 0; // Số ký tự riêng biệt
+
+//});
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseNotyf();
 app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.WebRootPath, "assets")), // Sử dụng WebRootPath để trỏ tới wwwroot
+    RequestPath = "/assets" // Đường dẫn để truy cập tài nguyên tĩnh
+});
 
+//app.UseStaticFiles(new StaticFileOptions
+//{
+//    FileProvider = new PhysicalFileProvider(
+//        Path.Combine(builder.Environment.ContentRootPath, "app")), // Sử dụng ContentRootPath
+//    RequestPath = "/app" // Đường dẫn để truy cập tài nguyên tĩnh
+//});
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "UploadFiles")),
+    RequestPath = "/UploadFiles"
+});
+
+app.UseSession();
 app.UseRouting();
-
-app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
