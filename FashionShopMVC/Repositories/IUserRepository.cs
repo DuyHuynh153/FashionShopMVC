302f9bc839bcbc794603e762a7e944de962a07af
thuan <thuanann.sama@gmail.com>
2024-10-14 09:49:16 +0700

    voucher DTO

---
+++ FashionShopMVC/Repositories/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using FashionShopMVC.Data;
+using FashionShopMVC.Models.Domain;
+
+namespace FashionShopMVC.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        //Task<IEnumerable<User>> GetUsersByRoleAsync(string roleName);
+        //Task<IEnumerable<User>> GetRoleNameByUser(string userName);
+    }
+
+    public class UserRepository : Repository<User>
+    {
+        public UserRepository(FashionShopDBContext context) : base(context) 
+        {
+
+        }
+    }
+}
